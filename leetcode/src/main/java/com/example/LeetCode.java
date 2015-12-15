39c3d0356cd11581607fc09d2c439000ab1a836f
majie <majie@idwzx.com>
2015-12-15 11:29:34 +0800

    update leetcode

--- leetcode/src/main/java/com/example/LeetCode.java
+++ leetcode/src/main/java/com/example/LeetCode.java
@@ -1,4 +1,38 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class LeetCode {
+    public static void main(String... args) {
+        //1 two sum
+        System.out.println("1.two sum = " + Arrays.toString(TwoSum.twoSum(new int[]{1, 2, 4, 5, 10, 15}, 9)));
+
+        //2 add two numbers
+        AddTwoNumbers.ListNode a = new AddTwoNumbers.ListNode(1);
+        a.next = new AddTwoNumbers.ListNode(2);
+        a.next.next = new AddTwoNumbers.ListNode(3);
+
+        AddTwoNumbers.ListNode b = new AddTwoNumbers.ListNode(9);
+        b.next = new AddTwoNumbers.ListNode(3);
+        b.next.next = new AddTwoNumbers.ListNode(5);
+
+        AddTwoNumbers.ListNode r2 = AddTwoNumbers.addTwoNumbers(a, b);
+        System.out.print("2.add two numbers = ");
+        while (r2 != null) {
+            System.out.print(r2.val + "->");
+            r2 = r2.next;
+        }
+        System.out.println("");
+
+        //3 Longest Substring Without Repeating Characters
+
+        //4 Median of Two Sorted Arrays
+
+        //5 longest palindromic substring
+        System.out.println("5.longest palindromic substring = " + LongestPalindromicSubstring.longestPalindrome("abcedfghjhg1ab"));
+
+        //6 ZigZag Conversion
+        System.out.println("6.zigzag conversion = " + ZigZagConversion.convert("abcdefghijklmnopq", 5));
+
+    }
 }
---
+++ leetcode/src/main/java/com/example/ZigZagConversion.java
@@ -0,0 +1,47 @@
+package com.example;
+
+import java.util.ArrayList;
+
+/**
+ * Created by majie on 15/12/15.
+ */
+public class ZigZagConversion {
+    /**
+     * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+     * <p/>
+     * P   A   H   N
+     * A P L S I I G
+     * Y   I   R
+     * And then read line by line: "PAHNAPLSIIGYIR"
+     * Write the code that will take a string and make this conversion given a number of rows:
+     * <p/>
+     * string convert(string text, int nRows);
+     * convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+     */
+
+    public static String convert(String s, int numRows) {
+        int length = s.length();
+        if (length <= numRows || numRows < 2) {
+            return s;
+        }
+        ArrayList<StringBuilder> list = new ArrayList<StringBuilder>();
+        for (int l = 0; l < numRows; l++) {
+            list.add(new StringBuilder());
+        }
+        for (int i = 0; i < length; i++) {
+            int index = (i + 1) % (2 * numRows - 2);
+            if (index == 0) {
+                list.get(1).append(s.charAt(i));
+            } else if (index <= numRows) {
+                list.get(index - 1).append(s.charAt(i));
+            } else {
+                list.get(2 * numRows - index - 1).append(s.charAt(i));
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (int l = 0; l < numRows; l++) {
+            result.append(list.get(l));
+        }
+        return result.toString();
+    }
+}
