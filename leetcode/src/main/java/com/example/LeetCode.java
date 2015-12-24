13212cfe399afdc340524c0453e2b79beaa43c7c
majie <majie@idwzx.com>
2015-12-24 17:30:12 +0800

    update 42

--- leetcode/src/main/java/com/example/LeetCode.java
+++ leetcode/src/main/java/com/example/LeetCode.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import com.example.pack1to50.FirstMissingPositive;
+import com.example.pack1to50.*;
 
 import java.util.Arrays;
 
@@ -120,8 +120,7 @@ public class LeetCode {
         com.example.pack1to50.NextPermutation.nextPermutation(new int[]{1, 2, 3, 5, 4});
         System.out.println("31. Next Permutation");
 
-        //32 Longest Valid Parentheses
-//        System.out.println("32 Longest Valid Prenthese " + LongestValidParentheses.longestValidParentheses("(()(((()"));
+        //32
 
         //33 Search in Rotated Sorted Array
         System.out.println("33. Search in Rotated Sorted Array " + com.example.pack1to50.SearchInARotateArray.search(new int[]{284, 287, 289, 293, 295, 298, 0, 3, 8, 9, 10, 11, 12, 15, 17, 19, 20, 22, 26, 29, 30, 31, 35, 36, 37, 38, 42, 43, 45, 50, 51, 54, 56, 58, 59, 60, 62, 63, 68, 70, 73, 74, 81, 83, 84, 87, 92, 95, 99, 101, 102, 105, 108, 109, 112, 114, 115, 116, 122, 125, 126, 127, 129, 132, 134, 136, 137, 138, 139, 147, 149, 152, 153, 154, 155, 159, 160, 161, 163, 164, 165, 166, 168, 169, 171, 172, 174, 176, 177, 180, 187, 188, 190, 191, 192, 198, 200, 203, 204, 206, 207, 209, 210, 212, 214, 216, 221, 224, 227, 228, 229, 230, 233, 235, 237, 241, 242, 243, 244, 246, 248, 252, 253, 255, 257, 259, 260, 261, 262, 265, 266, 268, 269, 270, 271, 272, 273, 277, 279, 281}, 235));
@@ -143,8 +142,11 @@ public class LeetCode {
 
         //40
 
-        //41
+        //41 First Missing Positive
         System.out.println("41. First Missing Positive " + FirstMissingPositive.firstMissingPositive(new int[]{1, 2, 3, 8, 9, 10, 5, 7, 4}));
+
+        //42 Trapping rain water
+        System.out.println("42. Trapping rain water " + TrappingRainWater.trap(new int[]{2, 1, 3}));
 
 
     }
